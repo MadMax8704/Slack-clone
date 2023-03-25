0880b90302675261a28c5a5b065653ba72b60a36
Norbert <norbrider87@gmail.com>
2023-03-25 10:54:12 +0100

    import authgard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,13 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
+import { AuthGuard } from './shared/guard/auth.guard';
 const routes: Routes = [
   { path: 'channel/:id', component: LandingPageComponent},
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'register-user', component: SignUpComponent },
-  { path: 'dashboard', component: LandingPageComponent },
+  { path: 'dashboard', component: LandingPageComponent , canActivate: [AuthGuard]} ,
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent }
 ];
