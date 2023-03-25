a1e4f0a07f987af2f57fe26a04827123ce45c5c5
Robert Hahn <robert.hahn91@gmx.at>
2023-03-25 10:48:23 +0100

    update MessageInput

--- src/app/message-input/message-input.component.ts
+++ src/app/message-input/message-input.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Channels } from 'src/models/channels';
 
 @Component({
   selector: 'app-message-input',
@@ -8,24 +10,49 @@ import { ActivatedRoute} from '@angular/router';
 })
 export class MessageInputComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, public firestore: AngularFirestore) { }
 
-  
+  id;
+  public channels: Channels = new Channels;
+  message;
 
   ngOnInit(): void {
     this.route.params.subscribe((params: any) => {
-      console.log('activatedRouteID' , this.route)
+      this.id = params.id;
     })
   }
 
 
-  sendMessage() {
-    
+  loadChat() {
+    this.route.params.subscribe((params: any) => {
+      this.id = params.id;
+    })
+    this.firestore
+      .collection('channels')
+      .doc(this.id)
+      .valueChanges()
+      .subscribe((channels: any) => {
+        // this.channels.message = channels.message;
+        // this.channels.channelId = channels.channelId;
+        // this.channels.channelName = channels.channelName;
+      })
   }
- 
 
 
- 
+  sendMessage() {
+    this.loadChat();
+    this.channels.message.push(this.message)
+    this.firestore
+      .collection('channels')
+      .doc(this.id)
+      .update(this.channels.toJson())
+    this.message = '';
+  }
+
+
+
+
+
 
   //GET CHANNEL ID FROM URL AND THEN PUSH INPUT INTO CHANNELS.MESSAGES!!!
 
