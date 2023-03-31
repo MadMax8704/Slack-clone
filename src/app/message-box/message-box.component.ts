45bf8903b500146f853e4ca51cddcad3be78dc22
Robert Hahn <robert.hahn91@gmx.at>
2023-03-31 09:20:57 +0200

    display messages from firebase

--- src/app/message-box/message-box.component.ts
+++ src/app/message-box/message-box.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Channels } from 'src/models/channels';
 
 @Component({
   selector: 'app-message-box',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageBoxComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public firestore: AngularFirestore) { }
+
+  id = '';
+  public channels: Channels = new Channels;
+
+
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      this.id = params.id;
+      this.firestore
+        .collection('channels')
+        .doc(this.id)
+        .valueChanges()
+        .subscribe((channels: any) => {
+          this.channels.message = channels.message;
+        })
+    })
   }
 
 }
