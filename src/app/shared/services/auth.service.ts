1e63264c26edce28aa562e60fce0670505c5b89f
Robert Hahn <robert.hahn91@gmx.at>
2023-04-13 16:50:58 +0200

    no input on welcomepage, show email if no username

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -118,6 +118,7 @@ export class AuthService {
       photoURL: user.photoURL,
       emailVerified: user.emailVerified,
     };
+    
     return userRef.set(userData, {
       merge: true,
     });
